c5a3ed1618d2165cef79e34d0121fc5ca172d70f
Taylor Nguyen <taylornguyen904@gmail.com>
2024-10-08 16:49:59 -0700

    tech stack layout done

--- obj/Debug/net8.0/PersonalWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5681974e51b7aca4a5a73f08411d33284746ebe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3b5865187c490454bf90600c56a64d4b88655a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
