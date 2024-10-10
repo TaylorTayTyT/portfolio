aeb87c59db1a6de5eedf60b736c9b57caf9f0c27
Taylor Nguyen <taylornguyen904@gmail.com>
2024-10-09 20:29:47 -0700

    before remodel

--- obj/Debug/net8.0/PersonalWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonalWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3b5865187c490454bf90600c56a64d4b88655a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a3ed1618d2165cef79e34d0121fc5ca172d70f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
